da8438c8d08145a3379659fb4191ea960f82665c
rraaffiii <rafi_lp@yahoo.com>
2020-07-18 22:58:36 +0600

    bug fixed

--- controllers/search.js
+++ controllers/search.js
@@ -54,6 +54,7 @@ router.get('/type/:type', async (req, res)=>{
 	const today = date.toISOString().slice(0, 10);
 	const tomorrow = new Date(date.setDate(date.getDate() + 1)).toISOString().slice(0,10);
 	
+	console.log(new Date(Date.now() - 1000*60).toDateString());
 	console.log(today);
 	console.log(tomorrow);
 	res.redirect('/search?location='+city+'&checkin='+today+'&checkout='+tomorrow+'&type='+req.params.type);	
